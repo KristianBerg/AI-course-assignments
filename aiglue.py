313ea005b2729b36806557ef3f1ca8c1c4306a01
Mecha_Magpie <dat13egr@student.lu.se>
2017-02-17 14:25:19 +0100

    fixes

--- aiglue.py
+++ aiglue.py
@@ -1,7 +1,8 @@
 import aiio
 import numpy as np
 import random as rand
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+import time
 
 (labels, datapoints) = aiio.to_batch(aiio.input_dense_LIBVSM('data'))
 
@@ -13,15 +14,18 @@ def normalize(feats):
 feats3 = normalize(datapoints)
 feats2 = normalize(np.vstack([feats3[0,:], feats3[2,:] / feats3[1,:]]))
 
-def train_stoch(perceptron, labels, feats, error_max):
-    while True:
-        ex = rand.randint(0, len(labels)-1)
-        perceptron.train(labels[ex], feats[:,ex])
-        if np.abs(labels - perceptron.batch_classify(feats)).sum() \
-           <= error_max * len(labels):
-            return
+learn_rate = 0.001
 
-def train_logit(logit, labels, feats, error_max):
+def train_stoch(perceptron, labels, feats, error_max = 0.1, epoch_max = 100):
+    for _ in range(epoch_max):
+        perm = np.random.permutation(np.vstack([labels,feats]).T)
+        for row in perm:
+            perceptron.train(row[0], row[1:], learn_rate / len(feats))
+        """if np.abs(labels - perceptron.batch_classify(feats)).sum() \
+           <= error_max * len(labels):
+            return"""
+
+def train_batch(logit, labels, feats, error_max):
     while True:
         logit.batch_train(labels, feats, 1)
         if np.abs(labels - logit.batch_classify(feats)).sum() \
@@ -33,19 +37,33 @@ def plot_prediction(labels, feats):
                   np.transpose([feats[1:,i] for i in range(len(labels)) if labels[i] == c])
     red = classfilter(0)
     blue = classfilter(1)
-    pyplot.plot(red[0], red[1], 'r.')
-    pyplot.plot(blue[0], blue[1], 'b.')
-
+    plt.plot(red[0], red[1], 'r.')
+    plt.plot(blue[0], blue[1], 'b.')    
     
-def plot_weights(weights):
-    line = np.transpose([[0,weights.dot([1,0])], [1, weights.dot([1,1])]])
-    pyplot.plot(line[0], line[1])
+def plot_3wts(weights):
+    line = [[0,1], [-weights.dot([1,0,0])/weights[2], -weights.dot([1,1,0])/weights[2]]]
+    plt.plot(line[0], line[1])
+
+def plot_2wts(weights):
+    plot_3wts(np.vstack(weights, ones(1))
 
 def plot(perceptron):
     plot_prediction(labels, feats3)
-    plot_weights(perceptron.weights)
-    pyplot.show()
+    plot_3wts(perceptron.weights)
 
+def error(perceptron, labels, features):
+    return np.abs(labels - perceptron.batch_classify(features)).sum()
+    
+    
 def iter(perceptron):
-    train_perceptron(perceptron, labels, feats2, 0.1)
-    plot(perceptron)
+    plt.ion()
+    fig = plt.figure()
+    for i in range(100):
+        train_stoch(perceptron, labels, feats3, epoch_max=1)
+        fig.canvas.draw()
+        plot(perceptron)
+        if error(perceptron, labels, feats3) == 0:
+            print(repr(i) + " epochs")
+            break
+    
+        
--- logit.py
+++ logit.py
@@ -6,8 +6,11 @@ class Logit(perceptron.Perceptron):
         return "1-layer logit net, weights: " + repr(self.weights)
     
     def batch_train(self, labels, examples, learn_rate):
-        self.weights += (learn_rate / len(labels)) * examples.dot(labels - 1 \
-            / (np.ones(len(labels)) + np.exp(-self.weights.dot(examples))))
+        return ((learn_rate / len(labels)) * 
+            examples.dot( 
+                labels - 1 / 
+                (np.ones(len(labels)) + np.exp(-self.weights.dot(examples))) 
+            ))
 
     def train(self, label, example, learn_rate = 1):
         self.weights += learn_rate * example * (label - 1 \
