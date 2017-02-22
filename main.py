cf6d6216a70ee00a3fcd5279353d54709fe81355
Kristian Berg <kristianberg.jobb@gmail.com>
2017-02-22 14:44:12 +0100

    modified stop condition to use change percentage instead of misclassified examples

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ log1 = logit.Logit(3)
 
 n_epoch_tron = 0
 while True:
-    aiglue.train_stoch(tron1, aiglue.labels, aiglue.feats3, learn_rate = 0.1)
+    aiglue.train_stoch(tron1, aiglue.labels, aiglue.feats3, learn_rate = 1)
     n_epoch_tron += 1
     if np.abs(aiglue.labels - tron1.batch_classify(aiglue.feats3)).sum() == 0:
         break
@@ -17,11 +17,13 @@ while True:
 n_epoch_logit = 0
 while True:
     old_wts = log1.weights.copy()
-    aiglue.train_stoch(log1, aiglue.labels, aiglue.feats3, learn_rate = 8)
+    aiglue.train_stoch(log1, aiglue.labels, aiglue.feats3, learn_rate = 1)
     n_epoch_logit += 1
-    if np.linalg.norm(log1.weights - old_wts) < 0.001:
+    if abs(np.linalg.norm(log1.weights - old_wts) / np.linalg.norm(log1.weights))  < 0.00001:
         break
 
+print('perceptron weights: ' + str(tron1.weights))
+print('logistic regression weights: ' + str(log1.weights))
 plt.subplot(211)
 aiglue.plot(tron1)
 plt.title('Perceptron (' + repr(n_epoch_tron) + ' epochs)')
